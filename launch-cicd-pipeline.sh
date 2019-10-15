f4d527617322cc3dcaa107a6381f06638f47a155
Ankush <32526153+ankushbraj@users.noreply.github.com>
2019-10-15 06:44:37 +0200

    Add files via upload

---
+++ launch-cicd-pipeline.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+AWS_DEFAULT_REGION="eu-central-1"
+Email="ankushbraj@gmail.com"
+
+# Parameters to Configure Specific Github Repo
+GitHub_User="ankushbraj"
+GitHub_Repo="ariane-test-c"
+GitHub_Branch="master"
+GitHub_Token="4c4faae8a5720cb32006a782db1a4ae974338ba1"
+
+# CodeBuild Project Parameters
+Python_Build_Version="aws/codebuild/python:3.6.5-1.3.2"
+Build_Timeout_Mins=30
+
+# SageMaker Training Job Parameters
+Instance_Count=1
+Instance_Type="ml.m4.xlarge"
+Max_Runtime_In_Seconds=86400
+Vol_In_GB=60
+
+Template_Name="${GitHub_Repo}-cicd-pipeline-c1"
+Lambdas_Bucket="${Template_Name}-lambdas-`date '+%Y-%m-%d-%H-%M-%S'`"
+Lambdas_Key="SageMakerTriggers/LambdaFunction.zip"
+
+cd lambda
+
+chmod -R 755 .
+
+zip -r ../LambdaFunction.zip .
+
+cd ..
+
+aws s3api create-bucket --bucket ${Lambdas_Bucket} --create-bucket-configuration LocationConstraint=eu-central-1
+
+aws s3api put-object --bucket ${Lambdas_Bucket} \
+  --key ${Lambdas_Key} \
+  --body ./LambdaFunction.zip
+
+aws cloudformation create-stack \
+  --region ${AWS_DEFAULT_REGION} \
+  --stack-name ${Template_Name} \
+  --template-body file://template/sagemaker-pipeline.yaml \
+  --parameters \
+    ParameterKey=LambdasBucket,ParameterValue=${Lambdas_Bucket} \
+    ParameterKey=LambdasKey,ParameterValue=${Lambdas_Key} \
+    ParameterKey=Email,ParameterValue=${Email} \
+    ParameterKey=GitHubUser,ParameterValue=${GitHub_User} \
+    ParameterKey=GitHubRepo,ParameterValue=${GitHub_Repo} \
+  	ParameterKey=GitHubBranch,ParameterValue=${GitHub_Branch} \
+  	ParameterKey=GitHubToken,ParameterValue=${GitHub_Token} \
+    ParameterKey=PythonBuildVersion,ParameterValue=${Python_Build_Version} \
+    ParameterKey=BuildTimeoutMins,ParameterValue=${Build_Timeout_Mins} \
+    ParameterKey=InstanceCount,ParameterValue=${Instance_Count} \
+    ParameterKey=InstanceType,ParameterValue=${Instance_Type} \
+    ParameterKey=MaxRuntimeInSeconds,ParameterValue=${Max_Runtime_In_Seconds} \
+    ParameterKey=VolInGB,ParameterValue=${Vol_In_GB} \
+  --capabilities CAPABILITY_NAMED_IAM
+
+rm -rf ./LambdaFunction.zip
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='classifytweet',
+    version='0.0.1rc0',
+
+    # Package data
+    packages=find_packages(),
+    include_package_data=True,
+
+    # Insert dependencies list here
+    install_requires=[
+        'numpy',
+        'scipy',
+        'pandas',
+        'nltk',
+        'Keras',
+        'tensorflow',
+        'matplotlib',
+        'flask',
+        'gevent',
+        'gunicorn',
+        'boto3'
+    ],
+
+    entry_points={
+       "classifytweet.training": [
+           "train=classifytweet.train:entry_point",
+       ],
+        "classifytweet.hosting": [
+           "serve=classifytweet.server:start_server",
+       ]
+    }
+)
+
+
+'''
+Traceback (most recent call last):
+File "/opt/run.py", line 1, in <module>
+from classifytweet.train import entry_point
+File "/usr/local/lib/python3.6/site-packages/classifytweet/train.py", line 14, in <module>
+from resolve import paths
+
+ModuleNotFoundError: No module named 'resolve'
+'''
